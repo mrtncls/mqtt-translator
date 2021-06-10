592b4998d9dc42918f69b678f111d38715836ee8
Gianfranco Di Prinzio <gianfrdp@inwind.it>
2021-06-10 15:56:05 +0200

    Added command line option to change log verbosity and modified setup.py to allow installing with ``` python3 setup.py install ```

--- mqtt_translator/__main__.py
+++ mqtt_translator/__main__.py
@@ -6,16 +6,26 @@ from mqtt_translator.bridge import Bridge
 
 def main():
 
-    parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(prog='mqtt-translator',
         description='MQTT topic translator which can act as a MQTT bridge.')
     parser.add_argument('-c', '--config-file', required=True,
                         help='Configuration YAML file')
     parser.add_argument('-v ', '--version',
                         action='version', version='%(prog)s 0.1')
+    parser.add_argument('-d ', '--debug', action="count", default=0,
+                        help='Increase log level, add more -d to increase verbosity')
+
     args = parser.parse_args()
 
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)s %(funcName)s: %(message)s', level=logging.DEBUG)
+    if args.debug >= 2:
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s %(funcName)s: %(message)s', level=logging.DEBUG)
+    elif args.debug >= 1:
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s %(funcName)s: %(message)s', level=logging.INFO)
+    else:
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s %(funcName)s: %(message)s', level=logging.WARN)
     logging.info('Loading setup...')
 
     with open(args.config_file, 'r') as stream:
